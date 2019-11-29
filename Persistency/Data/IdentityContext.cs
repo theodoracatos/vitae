636b673382c2634878d6dd6d2173903e5791ffb7
Alexandros Theodoracatos <theodoracatos@gmail.com>
2019-11-29 13:29:14 +0100

    Small change

--- Persistency/Data/IdentityContext.cs
+++ Persistency/Data/IdentityContext.cs
@@ -5,9 +5,14 @@ namespace Persistency.Data
 {
     public class IdentityContext : IdentityDbContext
     {
-        public IdentityContext(DbContextOptions<IdentityContext> options)
-            : base(options)
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
         {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=myDb;trusted_connection=true;");
         }
     }
 }
