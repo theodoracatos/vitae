53c46bc70fa693bb15ee864f769fa932844c685d
Alexandros Theodoracatos <theodoracatos@gmail.com>
2019-11-30 11:54:25 +0100

    Working QRCode

--- CVitae/Areas/CV/Pages/Index.cshtml.cs
+++ CVitae/Areas/CV/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QRCoder;
 
 namespace CVitae.Areas.CV.Pages
 {
@@ -11,10 +14,35 @@ namespace CVitae.Areas.CV.Pages
     public class IndexModel : PageModel
     {
         public Guid Guid { get; set; }
+        public string QRTag { get; set; }
 
         public void OnGet(Guid id)
         {
             Guid = id;
+            QRTag = CreateQRCode(id);
+        }
+
+
+
+        private string CreateQRCode(Guid id)
+        {
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                var qrCodeData = qrGenerator.CreateQrCode($"https//localhost/" + id.ToString(), QRCodeGenerator.ECCLevel.Q);
+                var qrCode = new QRCode(qrCodeData);
+                var bitmap = qrCode.GetGraphic(3);
+                var imageBytes = BitmapToBytes(bitmap);
+                return Convert.ToBase64String(imageBytes);
+            }
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
     }
 }
--- CVitae/wwwroot/js/resume.ts
+++ CVitae/wwwroot/js/resume.ts
@@ -1,5 +1,4 @@
-﻿"use strict"; // Start of use strict
-declare var $;
+﻿declare var $;
 
 $(function () {
 
