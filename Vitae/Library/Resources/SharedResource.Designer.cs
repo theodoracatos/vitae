2a6abf38f63fddfde348ca0d55ec3c94846add50
Alexandros Theodoracatos <theodoracatos@gmail.com>
2020-05-12 09:32:47 +0200

    Beta / preview ribbons added

--- Vitae/Library/Resources/SharedResource.Designer.cs
+++ Vitae/Library/Resources/SharedResource.Designer.cs
@@ -1348,7 +1348,7 @@ namespace Library.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Adiós.
+        ///   Looks up a localized string similar to Goodbye.
         /// </summary>
         public static string Goodbye {
             get {
@@ -1879,7 +1879,7 @@ namespace Library.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Your account was successfully deleted. We regret that you have left. However, you can register again at any time with this e-mail address.
+        ///   Looks up a localized string similar to Your account was successfully deleted. We regret that you have left. However, you can register again at any time with this e-mail address by.
         /// </summary>
         public static string MailBye3 {
             get {
@@ -2397,6 +2397,15 @@ namespace Library.Resources {
         public static string PleaseUpload {
             get {
                 return ResourceManager.GetString("PleaseUpload", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Preview only.
+        /// </summary>
+        public static string Preview {
+            get {
+                return ResourceManager.GetString("Preview", resourceCulture);
             }
         }
         
