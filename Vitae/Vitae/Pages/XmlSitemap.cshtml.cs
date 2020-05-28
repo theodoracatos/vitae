94aa7eed354c5120e7b9ab0f3a455681f51fff18
Alexandros Theodoracatos <theodoracatos@gmail.com>
2020-05-28 11:19:16 +0200

    Sitemap added

---
+++ Vitae/Vitae/Pages/XmlSitemap.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Library.Constants;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+namespace Vitae.Pages
+{
+    public class XmlSitemapModel : PageModel
+    {
+        public Dictionary<string, DateTime> VitaePages { get; private set; } = new Dictionary<string, DateTime>()
+        {
+            { $"{Globals.APPLICATION_URL}", DateTime.Now },
+            { $"{Globals.APPLICATION_URL}/{Globals.GENERAL_INFORMATION}", DateTime.Now },
+            { $"{Globals.APPLICATION_URL}/{nameof(Areas.Manage)}", DateTime.Now },
+            { $"{Globals.APPLICATION_URL}/{nameof(Areas.CV)}", DateTime.Now },
+            { $"{Globals.APPLICATION_URL}/{nameof(Areas.Identity)}", DateTime.Now }
+        };
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
